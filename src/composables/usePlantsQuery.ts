1275d566b7429c8c59e0ae0e63e419f3d0f12bfd
Madeleine Tee <madeleinetee@hotmail.com>
2026-03-24 18:43:29 +0800

    feat: execute query only when user is logged in

--- src/composables/usePlantsQuery.ts
+++ src/composables/usePlantsQuery.ts
@@ -1,8 +1,14 @@
 import { useQuery } from '@tanstack/vue-query'
 import { fetchPlants } from '@/models/plant'
+import { useFirebaseUser } from './useFirebaseUser'
+import { computed } from 'vue'
 
-export const usePlantsQuery = () =>
-    useQuery({
+export const usePlantsQuery = () => {
+    const { user } = useFirebaseUser()
+
+    return useQuery({
         queryKey: ['plants'],
+        enabled: computed(() => Boolean(user.value)),
         queryFn: fetchPlants
     })
+}
