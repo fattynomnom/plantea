72af8aaaf80d0860cc2a7f7c27f8d9f809d9a9d5
Madeleine Tee <madeleinetee@hotmail.com>
2026-09-02 23:37:37 +0800

    refactor: remove dead code

--- src/composables/useAreas.ts
+++ src/composables/useAreas.ts
@@ -1,15 +1,6 @@
 import { computed } from 'vue'
 import { usePlantsQuery } from './usePlantsQuery'
 import { useSetupsQuery } from './useSetupsQuery'
-import type { Plant } from '@/models/plant'
-
-const addPlantsCountInAreaMap = (acc: Record<string, number>, { area }: Pick<Plant, 'area'>) => {
-    const value = area?.trim()
-    if (value) {
-        acc[value] = (acc[value] ?? 0) + 1
-    }
-    return acc
-}
 
 export const useAreas = () => {
     const { data: plants } = usePlantsQuery()
